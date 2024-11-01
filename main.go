25ad3444ab47783a6d71dff06f635bed67fd13d1
Jakub Mesticky <mesticky.jakub7@gmail.com>
2024-11-01 09:45:36 +0100

    removing apis from main

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"signing-service-challenge/api"
 	"signing-service-challenge/persistence"
@@ -10,18 +9,11 @@ import (
 
 const (
 	ListenAddress = ":8080"
-	apis2         = "/api/v0/devices"
-	apis1         = "/api/v0/devices/list"
 )
 
 func main() {
 	store := persistence.NewInMemorys()
 	server := api.NewServer(ListenAddress, store)
-
-	deviceHandler := api.NewDeviceHandler(store)
-
-	http.HandleFunc(apis2, deviceHandler.CreateDevice)
-	http.HandleFunc(apis1, deviceHandler.CreateDevice)
 
 	if err := server.Run(); err != nil {
 		log.Fatal("Could not start server on ", ListenAddress)
